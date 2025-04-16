3d82ac5e48d4c807944e48ce98bb23799ef8e535
krupspurohit <krupapurohit99@gmail.com>
2025-04-16 17:26:30 +0530

    Folder & Ant UI Install

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      <div>Hello, VirenBhai, Demo CI.CD testing new data
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>Hello, VirenBhai, Demo CI.CD testing new data</div>
+		</>
+	);
 }
 
-export default App
+export default App;
